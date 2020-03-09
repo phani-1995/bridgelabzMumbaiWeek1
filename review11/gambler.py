eafa747f709157ef4ab185c1c44cf1b7f3c3658a
phani-1995 <phanindrajallavaram@gmail.com>
2020-03-09 09:28:24 +0530

    added:usecase3

--- review11/gambler.py
+++ review11/gambler.py
@@ -1,20 +1,34 @@
 import random
-def gambler(stake,num_of_times):
+def gambler(stake,num_of_times,days):
     bet=1
     win=loss=temp=0
     for i in range(1, num_of_times):
-        if (random.random() >= 0.5):
-            win = win + 1
-            stake = stake + bet
-        if (stake == goal):
-            break
-        else:
-            loss = loss + 1
-            stake = stake - bet
+        for j in range(1,days):
+            if (random.random() >= 0.5):
+                win = win + 1
+                stake = stake + bet
+            if (stake == goal):
+                break
+            else:
+                loss = loss + 1
+                stake = stake - bet
 
-        if (stake == stake // 2):
-            break
-            print("The game was completed for the day")
+            if (stake == stake // 2):
+                break
+                print("The game was completed for the day")
+
+            temp = i
+
+            if (temp <= days) and (stake == goal):
+                print("The goal has reached")
+            if (temp == days) and (stake != goal):
+                print("Timeup.... Goal not reached")
+            else:
+                print("No more stake")
+
+            print("No.of Wins:", win)
+            print("No.of Loss:", loss)
 stake = int(input("Enter the amount: "))
 num_of_times=int(input("Enter the number of times you want to play: "))
-gambler(stake,num_of_times)
+days=int(input("Enter the number of days: "))
+gambler(stake,num_of_times,days)
