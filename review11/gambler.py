d64fb513e90bf9129b8455cafa04ec617b6e205b
phani-1995 <phanindrajallavaram@gmail.com>
2020-03-09 09:13:25 +0530

    added:usecase2

--- review11/gambler.py
+++ review11/gambler.py
@@ -1,4 +1,14 @@
 import random
-def gambler():
+def gambler(stake,num_of_times):
     bet=1
-    stake=100
+    win=loss=temp=0
+    for i in range(1, num_of_times):
+        if (random.random() >= 0.5):
+            win = win + 1
+            stake = stake + bet
+
+        else:
+            loss = loss + 1
+            stake = stake - bet
+stake = int(input("Enter the amount: "))
+gambler(stake,num_of_times)
